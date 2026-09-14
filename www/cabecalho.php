30ec795201ef879546c0fa53ef3db7437069bb5b
Thaila <arholdthaila92@gmail.com>
2026-09-14 19:11:53 -0300

    cabeçalho e rodapé separados pra aproveitar o mesmo código nas outras telas

---
+++ www/cabecalho.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Abrigo de Animais</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <!-- cabeçalho -->
+    <header class="topo-navegacao">
+        <div class="container-header">
+            <div class="marca">
+                <a href="index.php">
+                    <img src="css/pata-de-cachorro.png" alt="Ícone Patinha" class="logo-icone">
+                    Lista Pets
+                </a>
+            </div>
+            
+            <nav class="menu-principal">
+                <ul>
+                    <li><a href="index.php">Pets</a></li>
+                    <li><a href="especies.php">Espécies</a></li>
+                    <li><a href="login.php" class="bot-sair">Desconectar</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
---
+++ www/rodape.php
@@ -0,0 +1,7 @@
+<!-- rodapé -->
+    <footer class="rodape-sistema">
+        <p>Sistema Cadastro Adoção Pets</p>
+    </footer>
+
+</body>
+</html>
