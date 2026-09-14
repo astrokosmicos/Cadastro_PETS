485939011fd95eafbcfcb38a84ec69c55f6ea85c
Thaila <arholdthaila92@gmail.com>
2026-09-14 18:54:40 -0300

    mudei a extensão de todos os HTML pra php, criei o arquivo docker-compose e o php para conectar. Também coloquei na pasta que precisa para o docker

---
+++ www/conecta.php
@@ -0,0 +1,12 @@
+<?php
+$servidor = "db";
+$usuario  = "root";
+$senha    = "";
+$banco    = "cadastros";
+
+$conexao = mysqli_connect($servidor, $usuario, $senha, $banco);
+
+if (!$conexao) {
+    die("Falha na conexão com o banco de dados: " . mysqli_connect_error());
+}
+?>
